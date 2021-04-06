b3efbe12fdfc33cabccdc0c15013982f82f346c2
thuyhang1602 <thuyhangmac2@gmail.com>
2021-04-06 13:28:39 +0700

    Fix send mail

--- api/v1/UserAPI.php
+++ api/v1/UserAPI.php
@@ -84,7 +84,7 @@ class UserAPI {
                         if($res->status){
                             $mail = new \mail\PHPMailer();
                             $mail->isSMTP();
-                            $mail->SMTPDebug  = 1;  
+                            $mail->SMTPDebug  = 3;  
                             $mail->SMTPAuth   = true;
                             $mail->SMTPSecure = "SSL";
                             $mail->Port       = 465;
